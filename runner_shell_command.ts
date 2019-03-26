83240bb8ba4182904f7a912705d39d4f39b40ba0
Valera Abornev <dr.webmaker@gmail.com>
2019-03-26 16:54:36 +0200

    added init, billing, instances list methods

--- runner_shell_command.ts
+++ runner_shell_command.ts
@@ -1,7 +1,12 @@
 import * as shell from 'shelljs';
+import * as _ from 'lodash';
 
-function runShellCommand() {
-    const command = 'git status';
+const context = {
+    PROJECT_ID: 'mongodb-cloud',
+    BILLING_ACCOUNT: '0X0X0X-0X0X0X-0X0X0X',
+};
+
+function runShellCommand(command) {
     let result = shell.exec(`${command}`);
     const {stderr, stdout} = result;
     if(result.code !== 0) {
@@ -10,4 +15,45 @@ function runShellCommand() {
     }
 }
 
-runShellCommand();
+function getProjectInstances(context) {
+    const {
+        PROJECT_ID
+    } = context;
+
+    const command = `gcloud projects list --filter="${PROJECT_ID}"`;
+    runShellCommand(command);
+}
+
+function createGcpProject(context) {
+    const {
+        PROJECT_ID
+    } = context;
+
+    const command = `gcloud projects create ${PROJECT_ID}`;
+    runShellCommand(command);
+}
+
+function linkProjectToBilling(context) {
+    const {
+        PROJECT_ID,
+        BILLING_ACCOUNT
+    } = context;
+
+    const command = `gcloud beta billing projects link ${PROJECT_ID} --billing-account=${BILLING_ACCOUNT}`;
+    runShellCommand(command);
+}
+
+function getGcpInstanceList() {
+    const command = `gcloud compute instances lis`;
+    runShellCommand(command);
+}
+
+
+const gcp_project_instances = getProjectInstances(context);
+
+if (_.isEmpty(gcp_project_instances)) {
+    createGcpProject(context);
+    linkProjectToBilling(context);
+    getGcpInstanceList;
+}
+
