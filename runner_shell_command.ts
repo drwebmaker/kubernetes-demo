94b9f50713b8f083910bb112af1e81069fff2d74
Valera Abornev <dr.webmaker@gmail.com>
2019-03-26 14:37:22 +0200

    error handling

--- runner_shell_command.ts
+++ runner_shell_command.ts
@@ -2,8 +2,12 @@ import * as shell from 'shelljs';
 
 function runShellCommand() {
     const command = 'git status';
-    let result = shell.exec(`${command}`, {silent:true}).stdout;
-    console.log(result);
+    let result = shell.exec(`${command}`);
+    const {stderr, stdout} = result;
+    if(result.code !== 0) {
+        shell.echo(stderr);
+        shell.exit(1);
+    }
 }
 
 runShellCommand();
